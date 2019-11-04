c41b86d49e6d09a35429945ad37874b8a73132a1
AnnaNgina <50610539+AnnaNgina@users.noreply.github.com>
2019-11-04 16:13:09 +0300

    correct

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
-import{ ProductDescriptionComponent } from './product-description/product-description.component';
-import{ ProductService } from './product.service';
+import { ProductDescriptionComponent } from './product-description/product-description.component';
+import { ProductService } from './product.service';
 
 
 @NgModule({
@@ -19,7 +19,7 @@ import{ ProductService } from './product.service';
     FormsModule,
     HttpModule
   ],
-  providers: [productService],
+  providers: [ProductService]
   bootstrap: [AppComponent]
 })
 export class AppModule { }
