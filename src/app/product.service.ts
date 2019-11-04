bca045bbbb13cc5beb75274c55d1161fe2b3fdb7
AnnaNgina <50610539+AnnaNgina@users.noreply.github.com>
2019-11-04 16:04:47 +0300

    corrections

---
+++ src/app/product.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+@ Injectable()
+export class ProductService{
+private_albumUrl='../assets/album.json';
+    constructor(private_http:Http){}
+    getAlbum(id: number){
+      return this._http.get(this._albumUrl).map((response))=>response.json());
+    }
+
+}
