5d89920e89bac7c5c81cfe8fde32c7df9fdb8001
AnnaNgina <50610539+AnnaNgina@users.noreply.github.com>
2019-11-04 17:04:42 +0300

    correction

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
-@ Injectable()
+@Injectable()
 export class ProductService{
 private_albumUrl='../assets/album.json';
-
-    constructor( private_http: Http ){}
+  constructor (private_http: Http ) { }
     getAlbum(id: number){
       return this._http.get(this._albumUrl).map((response))=>response.json());
     }
