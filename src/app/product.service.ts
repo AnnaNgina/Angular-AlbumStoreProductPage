8d35ec5cd673ffaff3fabebb9c8a521cbe6d7827
AnnaNgina <50610539+AnnaNgina@users.noreply.github.com>
2019-11-04 16:35:39 +0300

    Adding of a private _http:Http

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,7 +4,8 @@ import 'rxjs/add/operator/map';
 @ Injectable()
 export class ProductService{
 private_albumUrl='../assets/album.json';
-    constructor(private_http:Http){}
+
+    constructor( private_http:Http ){}
     getAlbum(id: number){
       return this._http.get(this._albumUrl).map((response))=>response.json());
     }
