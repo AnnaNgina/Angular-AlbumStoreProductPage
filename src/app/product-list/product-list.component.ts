e3f0a2f82a11cfa11e57e3d3faff487242ddfbd2
AnnaNgina <50610539+AnnaNgina@users.noreply.github.com>
2019-11-05 12:34:53 +0300

    corrections

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product} from '../product';
-import { ProductService} from '../productservice';
+import { ProductService} from '../product.service';
 
 @Component({
   selector: 'app-product-list',
