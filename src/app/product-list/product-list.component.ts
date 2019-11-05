0c4ce04f542b42603466b96ec29d9e006f6d8f99
AnnaNgina <50610539+AnnaNgina@users.noreply.github.com>
2019-11-05 14:40:50 +0300

    changes

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -11,7 +11,7 @@ import { ProductService} from '../product.service';
 export class ProductListComponent implements OnInit{
 
 products = Product[];
-  constructor(private _productservice :ProductService ){}
+  constructor(private _productService :ProductService ){}
   ngOnInit(){
 
   }
